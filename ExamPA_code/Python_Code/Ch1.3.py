02c1af0e9783aeab298292977509b0e49019e802
Lam Yan Ho <lam2002430@gmail.com>
2024-06-07 13:54:39 +0800

    Update Ch1.3.py

--- ExamPA_code/Python_Code/Ch1.3.py
+++ ExamPA_code/Python_Code/Ch1.3.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+import os
 actuary = pd.DataFrame({
     "x":[i for i in range(1,9)],
     "name": ["Embryo Luo", "", "Peter Smith", np.nan, "Angela Peterson", "Emily Johnston", "Barbara Scott", "Benjamin Eng"],
@@ -35,3 +35,39 @@ print(mean_of_Q1to3)
 #get best and worst actuary
 best_actuary = actuary_n["S"].max()
 worst_actuary = actuary_n["S"].min()
+actuary_n.sort_values(by = ["gender","S"],ascending=False,inplace=True)
+
+#ex 1.3.2
+actuary_FSA = actuary_n[actuary_n["exams"]==10]
+actuary_reduced = actuary_n[(actuary_n["gender"]=="M") | ((actuary_n["exams"]>=7) & (actuary_n["exams"]<=9))]
+actuary_reduced = actuary_reduced.sort_values(by = "exams",ascending=False)
+print(actuary_reduced)
+
+#Task 4(b)
+def get_title(data):
+    if data == 10:
+        return "FSA"
+    elif (data >=7) & (data <=9):
+        return "ASA"
+    else:
+        return "student"
+        
+actuary_n["title"] = actuary_n["exams"].apply(lambda x : get_title(x))
+print(actuary_n)
+
+#Task 5
+actuary_n = actuary_n.sort_index(ascending= True)
+new = pd.DataFrame({"smoke" : ["N", "N", "Y", "Y","N","N","N","Y"],
+                     "weight" : [70, 65, 60, 90, 60, 55, 50, 75]})
+
+new = new.iloc[actuary_n.index.tolist()]
+
+actuary_n = pd.concat([actuary_n,new],axis=1)
+print(actuary_n)
+
+# 8A
+import os
+import pandas as pd
+os.chdir("/workspaces/codespaces-jupyter/Projects/ExamPA/ExamPA_code/Python_Code")
+print(os.getcwd())
+college = pd.read_csv("College.csv")
